ed72bd9eef783317661b3e32fc070db90e5b6c11
Matt Barton <mbarton@embarkvet.com>
2016-04-02 22:49:03 -0400

    static requires, for browserify compatibility

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ var countries = require('./countries')
 
 var states = {}
 
-var normalizedPath = require('path').join(__dirname, 'states')
-
-require('fs').readdirSync(normalizedPath).forEach(function (file) {
-  require('./states/' + file)(states)
-})
+// static requires, to ensure this npm module will be compatible with browserify
+require('./states/AR')(states)
+require('./states/AU')(states)
+require('./states/BD')(states)
+require('./states/BG')(states)
+require('./states/BR')(states)
+require('./states/CA')(states)
+require('./states/CN')(states)
+require('./states/ES')(states)
+require('./states/GR')(states)
+require('./states/HK')(states)
+require('./states/HU')(states)
+require('./states/ID')(states)
+require('./states/IN')(states)
+require('./states/IR')(states)
+require('./states/IT')(states)
+require('./states/JP')(states)
+require('./states/MX')(states)
+require('./states/MY')(states)
+require('./states/NP')(states)
+require('./states/NZ')(states)
+require('./states/PE')(states)
+require('./states/PH')(states)
+require('./states/TH')(states)
+require('./states/TR')(states)
+require('./states/US')(states)
+require('./states/ZA')(states)
 
 module.exports = {
   countries: countries,
